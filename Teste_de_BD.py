443900a3f0d0da03a8a7696ae0a59376fdcdf4dc
Leonardo Rodrigues <Lthkrieger@gmail.com>
2025-03-28 21:34:34 -0300

    Reimplementado Requisito 2.

--- Teste_de_BD.py
+++ Teste_de_BD.py
@@ -3,28 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 import requests
 import time
 
 navegador = webdriver.Firefox()
+
 espera = WebDriverWait(navegador, 5)
+
 navegador.get("https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/")
 
 navegador.maximize_window()
 
 data = time.localtime().tm_year
 
-print(data)
-
 arquivos_ = {}
 
-Ultimos_dois_anos = [str(data - 1), str(data - 2)]
-
-print(Ultimos_dois_anos)
+ultimos_dois_anos = [str(data - 1), str(data - 2)]
 
 try:
-    for ano in Ultimos_dois_anos:
-        print(f"Procurando o ano de {ano}")
+    for ano in ultimos_dois_anos:
+
+        print(f"Buscando a pasta de {ano}...")
         pasta_selecionada = espera.until(
             EC.element_to_be_clickable((By.LINK_TEXT, f"{ano}/"))
         )
@@ -32,6 +32,7 @@ try:
 
         links = navegador.find_elements(By.TAG_NAME, "a")
 
+        print("Buscando todos os zip disponíveis...")
         arquivos_zip = [
             link.get_attribute("href")
             for link in links
@@ -42,14 +43,45 @@ try:
         arquivos_[ano] = arquivos_zip
 
         for link in arquivos_[ano]:
-            nome_arquivo =  os.path.basename(link)
+            nome_arquivo = os.path.basename(link)
 
             resposta = requests.get(link, stream=True)
             with open(nome_arquivo, "wb") as arquivo:
                 arquivo.write(resposta.content)
+                print(f"Baixando arquivo: {nome_arquivo}")
+            navegador.back()
+except Exception as err:
+    print("Erro ao baixar arquivos: {err}")
 
-except:
-    print("Erro")
+navegador.get(
+    "https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/"
+)
+
+try:
+    print(f"Buscando a pasta de Dados Cadastrais...")
+    pasta_selecionada = espera.until(
+        EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, f"dicionario_de_dados_.."))
+    )
+    link_dados = pasta_selecionada.get_attribute("href")
+
+    nome_arquivo = os.path.basename(link_dados)
+
+    resposta = requests.get(link_dados)
+
+    with open(nome_arquivo, "wb") as arquivo:
+        arquivo.write(resposta.content)
+        print(f"Baixando arquivo: {nome_arquivo}")
+
+    print("Convertendo para .ods ...")
+    df = pd.read_excel(nome_arquivo, engine="odf")
+    csv_path = nome_arquivo.replace(".odc", ".csv")
+    df.to_csv(csv_path, index=False, encoding="utf-8")
+
+    print("Arquivo convertido!")
+
+
+except Exception as err:
+    print(f"Erro ao baixar dados cadastrais: {err}")
 
 finally:
     navegador.quit()
--- Teste_de_TD.py
+++ Teste_de_TD.py
@@ -8,27 +8,47 @@ csv_file = "Rol_de_Procedimentos.csv"
 xlsx_file = "Rol_de_Procedimentos.xlsx"
 zip_file = "Teste_{Leonardo_Rodrigues}.zip"
 
-tabelas = tabula.read_pdf(pdf_file, pages="3-11")
 
-if tabelas:
-    df = pd.concat(tabelas, ignore_index=True)
-else:
-    print("Nenhuma tabela encontrada no PDF.")
-    exit()
+def extrair_tabela_pdf(pdf_file):
+    print("Lendo pdf...")
+    tabelas = tabula.read_pdf(pdf_file, pages="3-12") 
 
+    dataf = pd.concat(tabelas, ignore_index=True)
 
+    if len(dataf.columns) == 13:
+        dataf.columns = [
+            "PROCEDIMENTO",
+            "RN(alteração)",
+            "VIGÊNCIA",
+            "OD",
+            "AMB",
+            "HCO",
+            "HSO",
+            "REF",
+            "PAC",
+            "DUT",
+            "SUBGRUPO",
+            "GRUPO",
+            "CAPÍTULO",
+        ]
 
+    substituicoes = {"OD": "Odontológico", "AMB": "Ambulatorial"}
 
-substituicoes = {"OD": "Odontológico", "AMB": "Ambulatorial"}
-df.replace(substituicoes, inplace=True)
+    print("Atualizando campos...")
+    dataf.replace(substituicoes, inplace=True)
 
-df.to_csv(csv_file, index=False, encoding="utf-8")
-df.to_excel(xlsx_file,index=False, engine="openpyxl")
+    return dataf
 
-# 2.3 Compactar o CSV em um ZIP
-with ZipFile(zip_file, "w", ZIP_DEFLATED) as Zip:
-    Zip.write(csv_file, os.path.basename(csv_file))
-    Zip.write(xlsx_file,os.path.basename(xlsx_file))
+df_final = extrair_tabela_pdf(pdf_file)
 
+if df_final is not None:
+    print("Convertendo arquivos...")
+    df_final.to_csv(csv_file, index=False, encoding="utf-8")
+    df_final.to_excel(xlsx_file, index=False, engine="openpyxl")
 
-print(f"Arquivo ZIP criado: {zip_file}")
+    print("Compactando arquivos...")
+    with ZipFile(zip_file, "w", ZIP_DEFLATED) as Zip:
+        Zip.write(csv_file, os.path.basename(csv_file))
+        Zip.write(xlsx_file,os.path.basename(xlsx_file))
+
+    print(f"Arquivo ZIP criado: {zip_file}")
